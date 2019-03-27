3829a75f9f8904e949eebb53a6f9fd99a08ef3ff
Carlos <charlyras262@gmail.com>
2019-03-27 02:18:56 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+//Require the dependencies
+const express = require('express')
+const morgan = require('morgan');
+const database = require('./database');
+const passport = require('passport');
+const methodOverride = require('method-override');
+const app = express()
+
+//Setting
+app.set("port", process.env.PORT || 8080)
+
+//Database Connection
+database()
+
+//Middlerwares
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride('_method'));
+
+require('./controllers/validations/passport')(passport);
+
+//Routes
+app.use('/api/users', require('./routes/user.routes'));
+
+//Starting Server
+const server = require('http').Server(app)
+server.listen(app.get('port'), () => {
+    console.log(`Server is listening on port ${app.get('port')}`)
+})
+
